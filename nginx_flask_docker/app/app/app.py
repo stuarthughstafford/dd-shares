3bdafbea942717ffd2e8b2032463ca7de826694e
stuarthughstafford <45351824+stuarthughstafford@users.noreply.github.com>
2022-01-26 09:10:05 +0100

    Update app.py

--- nginx_flask_docker/app/app/app.py
+++ nginx_flask_docker/app/app/app.py
@@ -1,15 +1,32 @@
 from ddtrace import tracer, patch_all; patch_all(logging=True)
 from flask import Flask
 import logging
+import json_log_formatter
 
 app = Flask(__name__)
 
+# Set the logging so the traces and logs are correlated
+formatter = json_log_formatter.JSONFormatter()
+json_handler = logging.StreamHandler()
+json_handler.setFormatter(formatter)
+FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
+          '[dd.service=%(dd.service)s dd.env=%(dd.env)s dd.version=%(dd.version)s dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
+          '- %(message)s')
+logger = logging.getLogger('my_json')
+logger.addHandler(json_handler)
+logger.setLevel(logging.INFO)
 
 @app.route('/')
 def index():
-    # logger.info("This log is from the index route Hello, world" + logger.name)
+	#logger.info('test', extra={'test_json': 'hello world'})
+	#logger.info({"special": "value", "run": 12})
     return 'hello world!!'
 
-
+@app.route('/stu')
+def stu():
+    #logging.info("Stuart logging")
+    logger.info({"endpoint": "stuart", "check": "See if this message is correlated"})
+    return 'hello stu!!'
+	
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
